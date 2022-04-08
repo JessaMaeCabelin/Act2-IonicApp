8b183ee06782594cd8d895cb21d608fa04008f06
JessaMaeCabelin <jessamae.cabelin@evsu.edu.ph>
2022-04-09 01:23:31 +0800

    Activity 2 contents

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  contactlist  = [
+    {id: 1, name: 'Jessa Mae Cabelin', email: 'jessamae.cabelin@evsu.edu.ph',number: '09089145678'},
+    {id: 1, name: 'Coren Reboso', email: 'rebosocoren12@gmail.com',number: '09563490234'},
+    {id: 1, name: 'Alex Brother', email: 'cunaalex@gmail.com',number: '09666755484'},
+  ]
 
 }
